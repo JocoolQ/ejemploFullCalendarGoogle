dcfe6e6b6b61bd6f07b7d15dbe9a5f940a622453
Jonathan Steven Capera Quintana <jscaperaq@correo.udistrital.edu.co>
2020-10-15 14:36:01 -0500

    commit inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import googleCalendarPlugin from '@fullcalendar/google-calendar';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
+import { AppComponent } from './app.component';
+
+FullCalendarModule.registerPlugins([
+  dayGridPlugin,
+  googleCalendarPlugin,
+  timeGridPlugin,
+  listPlugin,
+  interactionPlugin
+])
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FullCalendarModule // import the FullCalendar module! will make the FullCalendar component available
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
